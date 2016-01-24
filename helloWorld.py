edebcd25666b1d47cadbe68467e6dc3428a77854
anishthite <anishthite@live.com>
2016-01-24 12:14:33 -0500

    Added Hello World program

---
+++ helloWorld.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+First thingamajig
+
+This is a temporary script file.
+"""
+"Watch Out World, Here I Come"
+print ("Watch Out World, Here I Come")
+(balloo) = "hello"
+print (ballo0)
