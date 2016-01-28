c524e6fc93b02a84e2af0b92df629bd7a0e825e9
anishthite <anishthite@live.com>
2016-01-27 20:57:23 -0500

    Update tip_calculator

---
+++ tip_calculator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 20:47:40 2016
+
+@author: Anish
+"""
+(tax) = .15
+(meal) = 44.50
+(tip) = .0675
+(tax_meal) = (meal) + (meal) * (tax)
+(total_meal) = (tax_meal) + (tax_meal)* (tax)
+print (total_meal) 
