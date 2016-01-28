368ebb4347c4081186a1b138e1e3b65d09860311
anishthite <anishthite@live.com>
2016-01-27 21:27:32 -0500

    Added Comments

--- Silly_Strings.py
+++ Silly_Strings.py
@@ -4,6 +4,15 @@ Created on Wed Jan 27 20:57:08 2016
 
 @author: Anish
 """
-
+#test out bug from 2.7 Python
 Anish = "it'sliteral's"
 print (Anish)
+
+#first try is a success!
+
+#test out way to see indexes
+
+(A) = ("anish" [0])
+print (A)
+
+#first try is a success!
--- tip_calculator.py
+++ tip_calculator.py
@@ -4,8 +4,15 @@ Created on Wed Jan 27 20:47:40 2016
 
 @author: Anish
 """
+
+"""
+
+A basic way to calculate a tip
+Input the tax, meal, and tip
+
+"""
 (tax) = .15
-(meal) = 44.50
+(meal) = 50.50
 (tip) = .0675
 (tax_meal) = (meal) + (meal) * (tax)
 (total_meal) = (tax_meal) + (tax_meal)* (tax)
