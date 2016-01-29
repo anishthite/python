91050f98e753d4cc273b48d82ca8ffbe733ec6b2
anishthite <anishthite@live.com>
2016-01-28 21:52:54 -0500

    Updates

--- Quadratic_Equation_Calculator.py
+++ Quadratic_Equation_Calculator.py
@@ -3,16 +3,15 @@ This is a calculator for the Quadratic Formula
 Enjoy
 """
 #Define input variables
-a=1 
-b=0
-c=0
+a = input("What's the A coeffitient?")
+b = input("What's the B coeffitient?")
+c = input("What's the C coeffitient?")
 
 #negate coeffitient B
 neg_b= b*-1
 
 #square B
-
-B_sqr= (b**2)
+B_sqr= b**2
 
 #Define the 4ac part
 
@@ -34,11 +33,12 @@ Answer_1= positive_root/2*a
 Answer_2= negative_root/2*a
 
 #Printing
-print ("Your A coeffitient is " + str(a) )
-print ("Your B coeffitient is " + str(b) )
-print ("Your C coeffitient is " + str(c) )
-print ("Answer 1 is " + str(Answer_1))
-print ("Answer 2 is " + str(Answer_2))
+print ("Note: j is the imaginary unit")
+print ("Your A coeffitient is: " + str(a) )
+print ("Your B coeffitient is: " + str(b) )
+print ("Your C coeffitient is: " + str(c) )
+print ("Answer 1 is: " + str(Answer_1))
+print ("Answer 2 is: " + str(Answer_2))
 
 
 
