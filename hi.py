939a320b965d2f029ce475f81d4e4664c6128597
anishthite <anishthite@live.com>
2016-01-28 19:46:46 -0500

    Update hi.py

--- hi.py
+++ hi.py
@@ -5,11 +5,26 @@ a=1
 b=2
 c=2
 neg_b= b*-1
-b^2= (b**2)
-print (b^2)
+
+B_sqr= (b**2)
+print (B_sqr)
 uio = 4*a*c
 radicand= b^2-uio
-print (raticand)
+print (radicand)
 
 negative_root= neg_b + 5
 positive_root=5
+
+
+
+
+
+Btwo= b**2
+print (Btwo)
+
+
+
+
+
+
+
