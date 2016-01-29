d44bc43aa5612b6b221a112655f557f92cad97e9
anishthite <anishthite@live.com>
2016-01-29 12:38:26 -0500

    Finished Quadratic_Equation_Calculator

--- Quadratic_Equation_Calculator.py
+++ Quadratic_Equation_Calculator.py
@@ -7,21 +7,15 @@ a = input("What's the A coeffitient?")
 b = input("What's the B coeffitient?")
 c = input("What's the C coeffitient?")
 
+
 #negate coeffitient B
-neg_b= b*-1
+neg_b= int(b)*-1
 
-#square B
-B_sqr= b**2
-
-#Define the 4ac part
-
-fourac = 4*a*c
 
 #creating the square root
 
-radicand_1= B_sqr-fourac
+radicand_1= (int(b)**2)-(4*int(a)*int(c))
 radicand_2= radicand_1**.5
-
 #creating the negative and positive options
 
 negative_root= neg_b - radicand_2
@@ -29,14 +23,14 @@ positive_root= neg_b + radicand_2
 
 #dividing by 2a
 
-Answer_1= positive_root/2*a
-Answer_2= negative_root/2*a
+Answer_1= positive_root/2*int(a)
+Answer_2= negative_root/2*int(a)
 
 #Printing
-print ("Note: j is the imaginary unit")
-print ("Your A coeffitient is: " + str(a) )
-print ("Your B coeffitient is: " + str(b) )
-print ("Your C coeffitient is: " + str(c) )
+print ("****Note: j is the imaginary unit****")
+print ("Your A coeffitient is: " + a)
+print ("Your B coeffitient is: " + b)
+print ("Your C coeffitient is: " + c)
 print ("Answer 1 is: " + str(Answer_1))
 print ("Answer 2 is: " + str(Answer_2))
 
