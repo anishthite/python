b9688b005f1b4fe4df87db67a365a501d899d2c1
anishthite <anishthite@live.com>
2016-02-15 17:13:48 -0500

    Used while loops to slow down computer

--- Py_Scripts/Anika.py
+++ Py_Scripts/Anika.py
@@ -1,13 +1,25 @@
-name = input("What is your name?")
-fav_color = input("What is your favorite fake color?")
 
-if (name) == "Anika" or "anika":
-    print ("YOU ARE TELLING THE TRUTH")
-elif (name) == "bob" or "bobilina":
-    print ("YOU ARE LYING")
-else:
-    print ("WHY ARE YOU LYING")
-if (fav_color) == "blue" or "purple" or "green":
-    print ("YOU ARE TELLING THE TRUTH")
-else:
-    print ("YOU SUCK")
+
+
+option = input("Input option- 1 is original program, 2 is the infinite loops")
+if int(option) = 1:
+    name = input("What is your name?")
+    fav_color = input("What is your favorite fake color?")
+
+        if (name) == "Anika" or "anika":
+            print ("YOU ARE TELLING THE TRUTH")
+        elif (name) == "bob" or "bobilina":
+            print ("YOU ARE LYING")
+        else:
+            print ("WHY ARE YOU LYING")
+        if (fav_color) == "blue" or "purple" or "green":
+            print ("YOU ARE TELLING THE TRUTH")
+        else:
+            print ("YOU SUCK")
+elif int(option) = 2:
+    print ("FEEL THE WRATH OF THE COMPUTER")
+    var = 0
+    while (var) == 0:
+    	import time
+    	time.sleep(.000000000000000005)
+    	print ("Mhahahahaha")
