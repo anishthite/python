84c5d43ac4fefa1b050e4151df9f2579ef228493
anishthite <anishthite@live.com>
2016-02-15 09:56:07 -0500

    created a new secret project

---
+++ Py_Scripts/76616C656E74696E65.py
@@ -0,0 +1,8 @@
+name = input("Well hey there, what is your name?")
+
+print ("Will you be my valentine " + name + "?")
+
+
+im = Image.open(r"C:\Users\Anish\Documents\python\Py_Scripts\76616C656E74696E65.png")
+
+print (im)
