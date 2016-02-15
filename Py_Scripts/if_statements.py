7f477a9325ce1944b0476392f375f677345de683
anishthite <anishthite@live.com>
2016-02-15 17:13:27 -0500

    Learnt while loops and added it in

--- Py_Scripts/if_statements.py
+++ Py_Scripts/if_statements.py
@@ -5,8 +5,16 @@
 
 if 5 > 0:
 	print ("Thite ")
-else:	
+else:
 	print ("yolo")
 
-	
+
 #success
+
+#creating infinite loops
+var = 0
+while (var) == 0:
+	print ("Hello")
+	import time
+	time.sleep(5)
+	print ("Bye")
