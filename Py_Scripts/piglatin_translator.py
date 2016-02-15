8fb3e0763b7b87189f4c5d649b1ed42b19c38411
anishthite <anishthite@live.com>
2016-02-15 17:13:13 -0500

    Tried to debug piglatin translator

--- Py_Scripts/piglatin_translator.py
+++ Py_Scripts/piglatin_translator.py
@@ -1,4 +1,10 @@
 #Piglatin translator
 
 original = input("Type or copy words into here")
-print (original)
+
+
+if len(original) = 0 and  original.isalpha() = False :
+    print ("Please type in something")
+    original = input("Try again here...")
+else:
+    print "Hold on, your text is translating"
