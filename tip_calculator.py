af002957a5979174449d68ea1a564a962b358f61
anishthite <anishthite@live.com>
2016-02-14 19:26:27 -0500

    Grammer changes

--- tip_calculator.py
+++ tip_calculator.py
@@ -11,9 +11,9 @@
 #Input the tax, meal, and tip
 
 
-tax = input("What's the tax? cost")
+tax = input("What's the tax in decimal form? ")
 meal = input("What's the meal cost?")
-tip = input("What's the tip cost?")
+tip = input("What's the tip in decimal form?")
 tax_meal = int(meal) + int(meal) * int(tax)
 total_meal = int(tax_meal) + int(tax_meal)* int(tax)
 print (total_meal) 
