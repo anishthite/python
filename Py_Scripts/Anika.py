40d7d8db2ba52d5bcdad42850b711b00989cef7f
anishthite <anishthite@live.com>
2016-02-15 10:13:52 -0500

    Created Anika.py as a joke

---
+++ Py_Scripts/Anika.py
@@ -0,0 +1,13 @@
+name = input("What is your name?")
+fav_color = input("What is your favorite fake color?")
+
+if (name) == "Anika" or "anika":
+    print ("YOU ARE TELLING THE TRUTH")
+elif (name) == "bob" or "bobilina":
+    print ("YOU ARE LYING")
+else:
+    print ("WHY ARE YOU LYING")
+if (fav_color) == "blue" or "purple" or "green":
+    print ("YOU ARE TELLING THE TRUTH")
+else:
+    print ("YOU SUCK")
