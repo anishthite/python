93beb36381962fe46cdb73d87569200edccac957
anishthite <anishthite@live.com>
2016-02-15 17:26:11 -0500

    Fixed Indenting/Spacing Anika.py

--- Py_Scripts/Anika.py
+++ Py_Scripts/Anika.py
@@ -5,16 +5,15 @@ option = input("Input option- 1 is original program, 2 is the infinite loops")
 if int(option) == 1:
     name = input("What is your name?")
     fav_color = input("What is your favorite fake color?")
-
-        if (name) == "Anika" or "anika":
+if (name) == "Anika" or "anika":
             print ("YOU ARE TELLING THE TRUTH")
-        elif (name) == "bob" or "bobilina":
+elif (name) == "bob" or "bobilina":
             print ("YOU ARE LYING")
-        else:
+else:
             print ("WHY ARE YOU LYING")
-        if (fav_color) == "blue" or "purple" or "green":
+if (fav_color) == "blue" or "purple" or "green":
             print ("YOU ARE TELLING THE TRUTH")
-        else:
+else:
             print ("YOU SUCK")
 elif int(option) = 2:
     print ("FEEL THE WRATH OF THE COMPUTER")
