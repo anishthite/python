069dc8becf2672f8603d57f3ffda101724882e14
anishthite <anishthite@live.com>
2016-02-15 20:56:49 -0500

    Updates

--- Py_Scripts/piglatin_translator.py
+++ Py_Scripts/piglatin_translator.py
@@ -3,7 +3,7 @@
 original = input("Type or copy words into here")
 
 
-if len(original) = 0 and  original.isalpha() = False :
+if len(original) == 0 and  original.isalpha() == False :
     print ("Please type in something")
     original = input("Try again here...")
 else:
