bc5e92b9248f4a3853c98cfb32b0ac475dbbaa8a
anishthite <anishthite@live.com>
2016-02-16 15:21:22 -0500

    added comments to code

--- Py_Scripts/Quadratic_Equation_Calculator.py
+++ Py_Scripts/Quadratic_Equation_Calculator.py
@@ -8,7 +8,11 @@
 #the A, B, and C coeffitients are the numbers inthe template above
 #when the computer asks for them, put them in
 #Enjoy
+
+#printing instructions
+
 print ("How to Use: put the equation into standard form (ax^2+bx+c) the A, B, and C coeffitients are the numbers inthe template above. When the computer asks for them, put them in. Let's Begin!")
+
 #Define input variables
 
 a = input("What's the A coeffitient?")
@@ -17,6 +21,7 @@ c = input("What's the C coeffitient?")
 
 
 #negate coeffitient B
+
 neg_b= int(b)*-1
 
 
@@ -35,7 +40,7 @@ positive_root= neg_b + radicand_2
 Answer_1= positive_root/2*int(a)
 Answer_2= negative_root/2*int(a)
 
-#Printing
+#Printing outputs
 print ("****Note: j is the imaginary unit****")
 print ("Your A coeffitient is: " + a)
 print ("Your B coeffitient is: " + b)
