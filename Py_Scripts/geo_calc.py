9e876f806188bb403ddd51242e3863dd7ce59f73
anishthite <anishthite@live.com>
2016-02-21 09:04:59 -0500

    Updates

--- Py_Scripts/geo_calc.py
+++ Py_Scripts/geo_calc.py
@@ -3,19 +3,22 @@
 
 
 
-#Getting input data
-
+#user input shape solving for:
 shape = input("What shape are you solving for?")
 shape_check = shape.lower()
+#changes to lower to check
 while 1==1:
+    #checks that they inputed right one
     if shape_check == "square" or "rectangle" or "triangle" or "trapezoid" or "circle":
         print ("You inputed %s " % (shape_check) )
         print ("Moving on...")
-
+        #user input: formula
         Formula = input("What formula are you solving for? Please input either Area or Perimeter")
+        # checks formula
         Formula_check = Formula.lower()
-
+        # changes to lower to check
         if Formula_check == "area" or "perimeter":
+            #one more check
             print ("You inputed %s " % (shape_check) )
                 if Formula_check == "area":
                     if shape check == "square" or "rectangle":
@@ -23,7 +26,7 @@ while 1==1:
                         width = input("What is the numerical width?")
                         unit = input("What is the unit of the length/width?")
                         answer = int(length) * int(width)
-                            if 
+                            if
                         print (answer)
                 if Formula_check == "perimeter":
         else:
