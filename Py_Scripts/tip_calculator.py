f05c1efa931ebd9f714806b6d8899e32a0e49fc8
anishthite <anishthite@live.com>
2016-02-22 22:09:40 -0500

    started v 2.0

--- Py_Scripts/tip_calculator.py
+++ Py_Scripts/tip_calculator.py
@@ -9,9 +9,11 @@
 #Input the tax, meal, and tip
 
 
-tax = input("What's the tax in decimal form? ")
+tax = input("What's the tax percentge? ")
+tax_dec = int(tax) /100
 meal = input("What's the meal cost?")
-tip = input("What's the tip in decimal form?")
+tip = input("What's the tip pecentage?")
+tip_dec = int(tip) / 100
 tax_meal = int(meal) + int(meal) * int(tax)
 total_meal = int(tax_meal) + int(tax_meal)* int(tax)
 print (total_meal)
