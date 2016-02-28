53ef1e3616f99e664e31c3df24d580750a464333
anishthite <anishthite@live.com>
2016-02-28 16:30:51 -0500

    Updates

---
+++ Project 1.py
@@ -0,0 +1,11 @@
+# An interest calculator FOR NOW
+#Author: Anish Thite, 2016
+#choose compound, consistently compund, or simple interest
+#getting Inerest Rate
+interest_rate =
+#Converting Interest Rate
+#Getting principle
+#converting principle
+#Getting time
+#converting time
+#COnverting formula
