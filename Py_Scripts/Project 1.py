4574da437a14714124be8d184d4ec898dc8966a0
anishthite <anishthite@live.com>
2016-03-11 22:01:14 -0500

    Reorganization

--- Py_Scripts/Project 1.py
+++ Py_Scripts/Project 1.py
@@ -1,13 +1,14 @@
 # An interest calculator FOR NOW
 #Author: Anish Thite, 2016
-#choose compound, consistently compund, or simple interest
-type = input("Choose method of interest: simple, compound, or consistently compounded")
-print type
+
 #getting Inerest Rate
 interest_rate = 2
 #Converting Interest Rate
 #Getting principle
 #converting principle
 #Getting time
+#choose compound, consistently compund, or simple interest
+type = input("Choose method of interest: simple, compound, or consistently compounded")
+print type
 #converting time
 #Converting formula
