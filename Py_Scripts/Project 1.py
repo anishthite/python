09d66bb73042683aaf5d55fb70a1cad3cad5ef4c
anishthite <anishthite@live.com>
2016-03-11 22:03:53 -0500

    Input added

--- Py_Scripts/Project 1.py
+++ Py_Scripts/Project 1.py
@@ -1,12 +1,15 @@
 # An interest calculator FOR NOW
 #Author: Anish Thite, 2016
-
+#welcome
+print ("Hey there, welcome to the Interest Guru! Let' Calculate your interest!")
 #getting Inerest Rate
-interest_rate = 2
+interest_rate = input("Please input your interest rate")
 #Converting Interest Rate
 #Getting principle
+interest_rate = input("Please input your interest rate")
 #converting principle
 #Getting time
+interest_rate = input("Please input your interest rate")
 #choose compound, consistently compund, or simple interest
 type = input("Choose method of interest: simple, compound, or consistently compounded")
 print type
