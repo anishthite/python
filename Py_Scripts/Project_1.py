4ff2c87242e6d52551cd6a3fe87aeb49d765e9b7
anishthite <anishthite@live.com>
2016-03-12 21:17:17 -0500

    updates

--- Py_Scripts/Project_1.py
+++ Py_Scripts/Project_1.py
@@ -2,20 +2,20 @@
 #Author: Anish Thite, 2016
 #welcome
 print ("Hey there, welcome to the Interest Guru! Let' Calculate your interest!")
-#getting Inerest Rate
+#getting Interest Rate
 interest_rate = input("Please input your interest rate")
 #Converting Interest
 interest_rate = float(interest_rate)
-x = interest_rate * .1
-
-
-#Getting principle
-#interest_rate = input("Please input your interest rate")
-#converting principle
-#Getting time
-#interest_rate = input("Please input your interest rate")
+#getting time
+interest_rate = input("Please input your interest rate")
+#Converting time
+interest_rate = float(interest_rate)
+#getting Principle
+interest_rate = input("Please input your interest rate")
+#Converting Principle
+interest_rate = float(interest_rate)
 #choose compound, consistently compund, or simple interest
-#type = input("Choose method of interest: simple, compound, or consistently compounded")
-#print type
+method = input("Choose method of interest: simple, compound, or consistently compounded")
+#print method
 #converting time
 #Converting formula
