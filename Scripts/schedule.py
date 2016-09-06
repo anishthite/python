6ff0673af7a706aca682e93adc89e03cfbfd2d6b
Anish Thite <anishthite@live.com>
2016-09-06 09:57:09 -0400

    added stuff

---
+++ Scripts/schedule.py
@@ -0,0 +1,15 @@
+schedule = {"APUSH": 1, "Spanish": 2, "Economics": 3, "Ap_Calc": 4, "Chemistry": 5, "Doctrine": 6 }
+day = input("What is the day?")
+
+if day == "Monday":
+    print ("hi")
+#elif day = "Tuesday":
+#print ("hi")
+#elif day == "Wednesday":
+#elif day == "Thursday":
+
+
+schedule["APUSH"] +=1
+
+
+print (schedule["APUSH"])
