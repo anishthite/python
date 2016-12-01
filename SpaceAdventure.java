da54c9a6a41cb76405928ff88711c353668d7ce4
Anish Thite <anishthite@live.com>
2016-12-01 10:22:02 -0500

    Update and rename adventure.java to SpaceAdventure.java

---
+++ SpaceAdventure.java
@@ -0,0 +1,22 @@
+//Anish Thite
+// Created for web gaming program
+import java.util.Scanner;
+
+public Scanner input = new Scanner(System.in);
+public int choice;
+public class AdventureGame{
+    public static void main(String[] args){
+        //print desc
+        //print choices
+        // read input
+        // check input
+        // go to different card
+        
+                
+        
+        
+    }
+    //card class or method
+
+
+}
