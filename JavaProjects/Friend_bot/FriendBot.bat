fbba8d6dafafe599f9f3b8e62cc9b90cee9be3e3
Anish Thite <Anish Thite>
2016-12-20 23:27:23 -0500

    updates

---
+++ JavaProjects/Friend_bot/FriendBot.bat
@@ -0,0 +1,1 @@
+java -jar FriendBot.jar
---
+++ JavaProjects/Friend_bot/src/JournalBot.java
@@ -0,0 +1,17 @@
+/** JournalBot- For use in FriendBot
+ * Saves text user asks to remember, creates running log w/ time
+ * @author Anish Thite, December 2016
+ * @version 1.0
+ * @since 12/20/2016
+ */
+public class JournalBot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
