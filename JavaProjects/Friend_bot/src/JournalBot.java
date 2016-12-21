2d9ed3dbd28f9d336d005d9698bd57344c023c76
Anish Thite <Anish Thite>
2016-12-21 00:07:31 -0500

    create file

--- JavaProjects/Friend_bot/src/JournalBot.java
+++ JavaProjects/Friend_bot/src/JournalBot.java
@@ -4,14 +4,27 @@
  * @version 1.0
  * @since 12/20/2016
  */
+import java.io.*;
 public class JournalBot {
 
+	private static FileOutputStream fos;
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		create_file();
 	}
-
+	/**Creates a journal (.txt file) if there exists no file
+	 * @throws IOException 
+	 * 
+	 */
+	public static void create_file() throws IOException{
+		 File test = new File("C:\\Users\\anish\\Documents\\GitHub\\python\\JavaProjects\\Friend_bot\\journaltest.txt");
+		 test.createNewFile();
+	}
+	/**appends the journal
+	 *  	
+	 */
+	public static void append(){}
 }
